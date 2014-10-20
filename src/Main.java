946ed5fce6f0b48010e2bb38bf201615fcae8da6
xmagam00 <xmagam00@stud.fit.vutbr.cz>
2014-10-20 22:29:51 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.logging.Logger;
+
+public class Main {
+
+    private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
