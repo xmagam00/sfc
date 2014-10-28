6a131d9cf9e62abae3bdb564feff57c0e1420f17
xmagam00 <xmagam00@stud.fit.vutbr.cz>
2014-10-28 11:29:58 +0100

    Create frame

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.logging.Logger;
 
+/**
+ * Created by <xmagam00@stud.fit.vutbr.cz> on 20.10.2014.
+ */
 public class Main {
 
-    private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
+    private static Logger LOG = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        LOG.info("Main method");
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(1000, 800);
+        frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
+
+
+
+
     }
 }
